87ce29e168b28ab3a7415e734c49eede85c9c531
Adam <ks20447@bristol.ac.uk>
2022-12-13 11:29:41 +0000

    Basic model done

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 # PROJECT AIM: Forcast bed occupancy based on distribution data and patient parameters
 # e.g Age, Area, Illness, Time of year (flu season etc.)
 
-# CURRENT ISSUES: using pyplot to generate counts is way too slow, need a better method - see line 62
-
 # !CAUTION! Colorama used to format output, may cause errors in other IDE's
 import random as rn
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from colorama import Fore, Style
 from numpy.random import choice
+
 
 # Simulation parameters
 AD_RATE = 55                # Patient admission rate (number per day)
@@ -18,7 +18,7 @@ ILLNESSES = {
     'Myocardial Infarction': [7.3, 6.5],
     'Stroke': [27.2, 29.2],
     'COVID 19': [15.7, 16.2],
-    'Fracture of Femur': [32.3, 28.8],                             # Dictionary of illnesses and their aLOS
+    'Fracture of Femur': [32.3, 28.8],                         # Dictionary of illnesses and their aLOS (male/female)
     'Atrial Fibrillation': [4.4, 4.1],
     'Pneumonia': [13.8, 16.0],
     'Endocrine, Nutritional and Metabolic Diseases': [4.0, 3.0],
@@ -33,6 +33,7 @@ class Hospital:
         self.beds = 300                                                 # Number of beds available
         self.patients_list = []                                         # List of all active patients
         self.occupancy = (len(self.patients_list) / self.beds)*100      # Percentage occupancy of beds
+        self.occupancy_timeline = np.zeros(SIM_TIME)
 
     def treat_patients(self, day):
         temp_patients = []
@@ -43,6 +44,19 @@ class Hospital:
         print(f"Number of patients discharged: {len(self.patients_list) - len(temp_patients)}")
         self.patients_list = temp_patients
         self.occupancy = len(self.patients_list) / self.beds * 100
+        self.occupancy_timeline[day] = self.occupancy
+
+    def show_results(self):
+        patient_data = []
+        for patient in self.patients_list:
+            patient_data.append(patient.__dict__)
+        df = pd.DataFrame(patient_data).drop('discharge_prob', axis=1)
+        print(df)
+        plt.plot(np.linspace(1, SIM_TIME, SIM_TIME), self.occupancy_timeline)
+        plt.xlabel(f"Day")
+        plt.ylabel(f"Bed Occupancy (%)")
+        plt.title(f"Hospital Bed Occupancy Forecast 3 Months in the Future")
+        plt.show()
 
 
 # Patient object to be treated by hospital
@@ -51,7 +65,7 @@ class Patient:
     def __init__(self, admission):
         self.gender = choice((0, 1), p=[0.5, 0.5])
         self.illness = rn.choice(list(ILLNESSES.keys()))
-        self.los = self.assign_a_los()
+        self.av_los = self.assign_a_los()
         self.discharge_prob = self.probability_distribution()
         self.ad_day = admission
 
@@ -60,28 +74,28 @@ class Patient:
         los = ILLNESSES[self.illness][self.gender]
         return los
 
-    # -NEEDS UPDATING TO EXPONENTIAL- Returns distribution of probability of leaving per day for given illness
-    # Commented out code is too slow
+    # Returns distribution of probability of leaving per day for given illness
     def probability_distribution(self):
-        dist = np.random.normal(self.los, 10, size=100)
-        # dist = np.random.exponential(self.los, size=100)
-        # counts = plt.hist(dist, density=True, cumulative=True, bins=int(max(dist)))
-        return dist     # counts[0]
+        dist = sorted(np.random.exponential(self.av_los, size=100))
+        dist = np.cumsum(dist)
+        dist = dist/dist[-1]
+        return dist
 
 
 # Runs the simulation using simulation parameters
 def run(days, ad_rate):
     hospital = Hospital()
     for i in range(days):
-        print(f"Day {i}")
+        print(f"Day {i+1}")
         for j in range(ad_rate + 1):
-            if len(hospital.patients_list) < 300:           # Checks bed occupancy hasn't been reached
-                hospital.patients_list.append(Patient(i))   # Patients get admitted if there is an available bed
+            if len(hospital.patients_list) < hospital.beds:         # Checks bed occupancy hasn't been reached
+                hospital.patients_list.append(Patient(i))  # Patients get admitted if there is an available bed
             else:
                 break
         print(f"Number of patients admitted: {j}")
         hospital.treat_patients(i)
         print(Fore.BLUE + f"Occupancy: {hospital.occupancy:.1f}%" + Style.RESET_ALL)
+    hospital.show_results()
 
 
 if __name__ == '__main__':
